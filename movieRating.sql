6f1bac6c7edd31d88a0d7d67f12f94d48f65bc27
Namish Arora <namish.arora@gmail.com>
2024-03-27 17:25:47 -0400

    movieRating.sql
    Solution for "Movie Rating" Leetcode problem

---
+++ movieRating.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+(Select u.name as results from MovieRating m, Users u
+where u.user_id = m.user_id 
+group by m.user_id
+order by COUNT(rating) desc, u.name
+limit 1)
+union all
+(Select title as results from MovieRating r, Movies m
+where r.movie_id = m.movie_id and MONTH(created_at)=2 and YEAR(created_at) = 2020
+group by r.movie_id
+order by AVG(rating) desc,m.title
+limit 1)
+
+
+
+
