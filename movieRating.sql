30fc58c6d241e2daa7e9db0425068c69eb6183d2
Namish Arora <namish.arora@gmail.com>
2024-04-03 15:51:07 -0400

    updates

--- movieRating.sql
+++ movieRating.sql
@@ -5,7 +5,7 @@ group by m.user_id
 order by COUNT(rating) desc, u.name
 limit 1)
 union all
-(Select title as results from MovieRating r, Movies m
+(Select title as results from MovieRating r, Movies mr
 where r.movie_id = m.movie_id and MONTH(created_at)=2 and YEAR(created_at) = 2020
 group by r.movie_id
 order by AVG(rating) desc,m.title
